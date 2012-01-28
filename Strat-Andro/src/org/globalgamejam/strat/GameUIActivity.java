acb94686f809c6f81e61511659a3bca873cc034e
JackDesBwa <jackdesbwa@gmail.com>
2012-01-28 20:43:48 +0100

    Communication is happy

--- Strat-Andro/src/org/globalgamejam/strat/GameUIActivity.java
+++ Strat-Andro/src/org/globalgamejam/strat/GameUIActivity.java
@@ -1,20 +1,23 @@
 package org.globalgamejam.strat;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 
-
 public class GameUIActivity extends AndroidApplication {
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		
-		initialize(new GameRenderer(), false);
+
+		// Log.d("aa", savedInstanceState.getString("host"));
+
+		// initialize(new GameRenderer(savedInstanceState.getString("host"),
+		// savedInstanceState.getInt("port")), false);
+		initialize(new GameRenderer("192.168.1.77", 50000), false);
 	}
 	/** Called when the activity is first created. */
-	
-	
+
 }
